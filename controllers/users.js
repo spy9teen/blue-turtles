d7b1cedd0d795d898d1c888800ea846e75acea20
Vlad M <45720348+spy9teen@users.noreply.github.com>
2020-02-07 10:07:26 +0300

    test CUD methods

--- controllers/users.js
+++ controllers/users.js
@@ -9,15 +9,18 @@ class UserController {
         res.send(await new User('Users').getElement(req.params.id))
     }
     
-    static create(req, res) {
-        //res.send(await User.create(req))
+    static async create(req, res) {
+        //res.send(await User.create(req));
+        res.send({test: true, message: 'user CREATE controller'});
     }
 
-    static update(req, res) {
-        // res.send(await User.save())
+    static async update(req, res) {
+        // res.send(await User.save());
+        res.send({test: true, message: 'user UPDATE controller'});
     }
-    static delete(req, res) {
-        // res.send(await User.delete(req.params.id))
+    static async delete(req, res) {
+        // res.send(await User.delete(req.params.id));
+        res.send({test: true, message: 'user DELETE controller'});
     }
 }
 
