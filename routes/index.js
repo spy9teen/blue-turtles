3f6accd715da56e2966da751e81966d0516e7681
spy9teen <spy19th@gmail.com>
2020-02-17 15:15:52 +0300

    clear all files

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.all('/', function(req, res, next) {
-  res.send({ title: 'Express' });
-});
-
-module.exports = router;
